dc4d40a9e1d3e47f8ebebfefba6b3fe92ed20ce4
Koushik Dutta <koushd@gmail.com>
2012-02-22 23:37:59 -0800

    sync

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env sh
 
+REPO=$(which repo)
+if [ -z "$REPO" ]
+then
+  mkdir -p ~/bin
+  curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > ~/bin/repo
+  chmod a+x ~/bin/repo
+fi
+
+if [ ! -z "$REPO_BRANCH" ]
+then
+  echo REPO_BRANCH not specified
+  exit 1
+fi
+
+if [ ! -d $REPO_BRANCH ]
+then
+  mkdir $REPO_BRANCH
+  cd $REPO_BRANCH
+  repo init -u git://github.com/CyanogenMod/androig.it -b $REPO_BRANCH
+else
+  cd $REPO_BRANCH
+fi
+
+repo sync
+
 echo success!
