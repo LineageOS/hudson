1e38b34fb63317e592f3499e265a634e7d9a6bbe
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-04-18 15:08:47 +0100

    Calculate home for daemonized jenkins slaves
    When started by init, jenkins doesn't have a $HOME env var, which
    is needed by git. Calculate one from $USER (via /etc/passed)

--- build.sh
+++ build.sh
@@ -8,6 +8,12 @@ function check_result {
   fi
 }
 
+if [ -z "$HOME" ]
+then
+  echo HOME not in environment, guessing...
+  export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
+fi
+
 if [ -z "$WORKSPACE" ]
 then
   echo WORKSPACE not specified
--- job.sh
+++ job.sh
@@ -1,3 +1,9 @@
+if [ -z "$HOME" ]
+then
+  echo HOME not in environment, guessing...
+  export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
+fi
+
 cd $WORKSPACE
 mkdir -p ../android
 cd ../android
