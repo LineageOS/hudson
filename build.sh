2af07e2775aeeeaf8ba7d54f292d34c054ecdd7f
Chirayu Desai <chirayudesai1@gmail.com>
2012-08-22 12:12:40 +0530

    derp

--- build.sh
+++ build.sh
@@ -164,7 +164,7 @@ then
   fi
 fi
 
-if [ ! "$(ccache -s|grep -E 'max cache size'|awk '{print $4}')" = "50.0" ]
+if [ ! "$(ccache -s|grep -E 'max cache size'|awk '{print $4}')" = "200.0" ]
 then
   ccache -M 200G
 fi
