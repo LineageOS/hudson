b212a40899e91d8c3868f4021624e0eb0760713d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-10-03 14:49:23 +0100

    changes: prettify the output a bit

--- changes/buildlog.sh
+++ changes/buildlog.sh
@@ -10,15 +10,14 @@ prevts=
 for ts in `python2 $MYPATH/getdates.py $CM_BUILD`; do
 
 export ts
-(echo "============================="
-date -d @$ts 
-echo "============================="
+(echo "==================================="
+echo -n "Since ";date -d @$ts 
+echo "==================================="
 if [ -z "$prevts" ]; then
-  repo forall -c 'L=$(git log --oneline --no-merges --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --no-merges --since $ts; echo; fi'
+  repo forall -c 'L=$(git log --oneline --no-merges --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --no-merges --since $ts; fi'
 else
-  repo forall -c 'L=$(git log --oneline --no-merges --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --no-merges --since $ts --until $prevts; echo; fi'
+  repo forall -c 'L=$(git log --oneline --no-merges --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --no-merges --since $ts --until $prevts; fi'
 fi
-echo "============================="
 echo) >> $WORKSPACE/archive/CHANGES.txt
 export prevts=$ts
 
