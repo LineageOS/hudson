0dbf1704f8b1733a4205618c0a49451f4bc1e212
Steve Kondik <shade@chemlab.org>
2012-11-13 02:20:12 -0800

    Add support for release builds using consistent branch.

--- build.sh
+++ build.sh
@@ -92,7 +92,7 @@ repo init -u $SYNC_PROTO://github.com/CyanogenMod/android.git -b $CORE_BRANCH
 check_result "repo init failed."
 
 # make sure ccache is in PATH
-if [ "$REPO_BRANCH" == "jellybean" ]
+if [ "$REPO_BRANCH" == "jellybean" ] || [ "$REPO_BRANCH" == "jellybean-stable" ]
 then
 export PATH="$PATH:/opt/local/bin/:$PWD/prebuilts/misc/$(uname|awk '{print tolower($0)}')-x86/ccache"
 export CCACHE_DIR=~/.jb_ccache
---
+++ jellybean-stable-setup.sh
@@ -0,0 +1,1 @@
+vendor/cm/get-prebuilts
