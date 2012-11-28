f87eb9c1c1cc545328603149b785187356e35e73
Koushik Dutta <koushd@gmail.com>
2012-11-28 13:37:38 -0800

    derp

--- build.sh
+++ build.sh
@@ -198,7 +198,7 @@ if [ $TIME_SINCE_LAST_CLEAN -gt "24" -o $CLEAN_TYPE = "clobber" ]
 then
   echo "Cleaning!"
   touch .clean
-  make clobber
+  make $CLEAN_TYPE
 else
   echo "Skipping clean: $TIME_SINCE_LAST_CLEAN hours since last clean."
 fi
