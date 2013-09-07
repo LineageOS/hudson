4eb7ddbeb477ee0eac46cf57739229e5d167e1a7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-07 15:03:09 +0100

    Explicitly ask for master branches
    something's going on at github, it's returning errors for all non-explicit
    branch checkouts
    
    $ git clone https://github.com/CyanogenMod/hudson
    Cloning into 'hudson'...
    remote: Counting objects: 1409, done.
    remote: Compressing objects: 100% (835/835), done.
    remote: Total 1409 (delta 751), reused 1206 (delta 560)
    Receiving objects: 100% (1409/1409), 232.30 KiB | 233 KiB/s, done.
    Resolving deltas: 100% (751/751), done.
    warning: remote HEAD refers to nonexistent ref, unable to checkout.

--- build.sh
+++ build.sh
@@ -134,7 +134,7 @@ mkdir -p .repo/local_manifests
 rm -f .repo/local_manifest.xml
 
 rm -rf $WORKSPACE/build_env
-git clone https://github.com/CyanogenMod/cm_build_config.git $WORKSPACE/build_env
+git clone https://github.com/CyanogenMod/cm_build_config.git $WORKSPACE/build_env -b master
 check_result "Bootstrap failed"
 
 if [ -f $WORKSPACE/build_env/bootstrap.sh ]
