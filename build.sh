ed61e6bce3b692c37f39cf72ef415e7c9f7bd8b8
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-06 21:31:58 +0000

    Generate the debug bootimage after archiving the build.prop
    We don't want to store a build.prop with the wrong tags...

--- build.sh
+++ build.sh
@@ -361,6 +361,10 @@ then
   cp $OUT/recovery.img $WORKSPACE/archive
 fi
 
+# archive the build.prop as well
+ZIP=$(ls $WORKSPACE/archive/cm-*.zip)
+unzip -p $ZIP system/build.prop > $WORKSPACE/archive/build.prop
+
 if [ "$TARGET_BUILD_VARIANT" = "user" -a "$EXTRA_DEBUGGABLE_BOOT" = "true" ]
 then
   # Minimal rebuild to get a debuggable boot image, just in case
@@ -371,10 +375,6 @@ then
   check_result "Failed to generate a debuggable bootimage"
   cp $OUT/boot.img $WORSPACE/archive/boot-debuggable.img
 fi
-
-# archive the build.prop as well
-ZIP=$(ls $WORKSPACE/archive/cm-*.zip)
-unzip -p $ZIP system/build.prop > $WORKSPACE/archive/build.prop
 
 # Build is done, cleanup the environment
 cleanup
